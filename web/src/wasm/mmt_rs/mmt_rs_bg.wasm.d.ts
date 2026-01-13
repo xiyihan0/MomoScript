796767e77ed61ae9bfc2b1732750c9e181d76621
xiyihan <1904534671@qq.com>
2026-01-13 12:59:39 +0800

    fix: include wasm artifacts for netlify

---
+++ web/src/wasm/mmt_rs/mmt_rs_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const compile_text_wasm: (a: number, b: number) => [number, number, number, number];
+export const compile_text_with_options_wasm: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const compile_text_with_pack_wasm: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number) => [number, number, number, number];
+export const compile_text_with_pack_and_options_wasm: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number) => [number, number, number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
