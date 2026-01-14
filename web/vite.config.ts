c0a6c8ed5680132b0749ee2efbf37fc3306a919e
xiyihan <1904534671@qq.com>
2026-01-14 13:14:30 +0800

    chore(web): enable sourcemap generation for debugging

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   server: {
     fs: {
       allow: [
